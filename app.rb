3daed66dfe4f46d84ee2919b8d34cc5353bb1ccc
Chris Mitchell <chrisjmit@gmail.com>
2017-04-26 12:59:50 +0100

    Test for name added

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
 
+
+
 class Battle < Sinatra::Application
   get '/' do
     'Testing infrastructure working!'
+  end
+
+  get '/' do
+    erb :index
   end
 
 run! if app_file == $0
--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -1,6 +1,22 @@
 feature "Testing infrastructure" do
+
   scenario "Can run app and check page content" do
     visit("/")
-  expect(page).to have_content "Testing infrastructure working!"
+    expect(page).to have_content "Testing infrastructure working!"
   end
+
+  scenario "Filling in names in a form" do
+    visit('/')
+    fill_in :name, with: "Chris"
+    click_button "Submit"
+      expect(page).to have_content "Chris"
+  end
+
+  # p feature "table flip a struggle" do
+  # p  scenario "submit 'Programming' as a struggle and see the table get flipped" do
+  # p    visit("/")
+  # p   fill_in :struggle, with: "Programming"
+  # p   click_button "Flip table"
+  # p     expect(page).to have_content "(Programming!!!)> (╯°□°）╯︵ ┻━┻"
+
 end
