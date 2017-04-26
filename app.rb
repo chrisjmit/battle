e2dffb096b9b68485c4971451789920d34638266
Sophie Mann <sklm@live.co.uk>
2017-04-26 14:27:04 +0100

    adds player name form and post names route

--- app.rb
+++ app.rb
@@ -3,12 +3,16 @@ require 'sinatra/base'
 
 
 class Battle < Sinatra::Application
-  get '/' do
-    'Testing infrastructure working!'
-  end
 
   get '/' do
     erb :index
+  end
+
+  post '/names' do
+    p params
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
   end
 
 run! if app_file == $0
--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -1,15 +1,12 @@
 feature "Testing infrastructure" do
 
-  scenario "Can run app and check page content" do
-    visit("/")
-    expect(page).to have_content "Testing infrastructure working!"
-  end
-
   scenario "Filling in names in a form" do
     visit('/')
-    fill_in :name, with: "Chris"
+    fill_in :player_1_name, with: "Chris"
+    fill_in :player_2_name, with: "Sophie"
     click_button "Submit"
-      expect(page).to have_content "Chris"
+    expect(page).to have_content "Chris"
+    expect(page).to have_content "Sophie"
   end
 
   # p feature "table flip a struggle" do
