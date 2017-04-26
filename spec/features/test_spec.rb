fac511cd976643a77d4a46b6cd623caa8061b843
Sophie Mann <sklm@live.co.uk>
2017-04-26 16:00:22 +0100

    adds hit point test with minimal implementation

--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -9,4 +9,12 @@ feature "Testing infrastructure" do
     expect(page).to have_content "Sophie"
   end
 
+  scenario "Viewing hit points" do
+    visit('/')
+    fill_in :player_1_name, with: "Sophie"
+    fill_in :player_2_name, with: "Chris"
+    click_button "Submit"
+    expect(page).to have_content("Chris: 60 HP")
+  end
+
 end
