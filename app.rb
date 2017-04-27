ec3e80d1420a85cfa6677946b708e7a5446321b8
Chris Mitchell <chrisjmit@gmail.com>
2017-04-27 15:40:47 +0100

    app.rb updated to use instance vars for /attack

--- app.rb
+++ app.rb
@@ -23,8 +23,8 @@ class Battle < Sinatra::Application
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
     Game.new.attack(@player_2)
     erb :attack
   end
