3b92b5f869b3392f360a903ea9334d3d82cd42af
Chris Mitchell <chrisjmit@gmail.com>
2017-04-27 14:50:22 +0100

    Attack moved to game class

--- app.rb
+++ app.rb
@@ -24,7 +24,8 @@ class Battle < Sinatra::Application
 
   get '/attack' do
     @player_1_name = $player_1.name
-    @player_2_name = $player_2.name 
+    @player_2_name = $player_2.name
+    Game.new.attack(@player_2)
     erb :attack
   end
 
