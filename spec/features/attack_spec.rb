8397823ac846daf26a479580022e0d879cd47661
Chris Mitchell <chrisjmit@gmail.com>
2017-04-27 18:06:41 +0100

    Minimising the controller content, 2 tests failing

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -5,12 +5,12 @@ feature "Attacking" do
     scenario "Attacking player 2" do
       sign_in_and_play
       click_button("Attack")
-      expect(page).to have_content("#{@player_1_name} attacked #{@player_2_name}!!!")
+      expect(page).to have_content("#{@game.player_1.name} attacked #{@game.player_2.name}!!!")
     end
-
+    
     scenario "reduce player2 hit points by 10" do
       sign_in_and_play
       click_button("Attack")
-      expect(page).to have_content("#{@player_2_name}: 90HP")
+      expect(page).to have_content("#{@game.player_2.name}: 90HP")
     end
   end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,6 @@
 feature "Hit points" do
   scenario "Viewing hit points" do
     sign_in_and_play
-  expect(page).to have_content("Ben: 60 HP")
+  expect(page).to have_content("Ben: 100HP")
   end
 end
