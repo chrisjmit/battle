b344b16d08107e239ec0c666bc15426912561871
Chris Mitchell <chrisjmit@gmail.com>
2017-04-28 11:16:43 +0100

    Skinny controller refactored to use players at model layer

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,16 +1,19 @@
 require 'game'
 
+#let(:player_1) { double :player, name: "player_1"}
+#let(:player_2) { double :player, name: "player_2"}
+
 feature "Attacking" do
 
     scenario "Attacking player 2" do
       sign_in_and_play
       click_button("Attack")
-      expect(page).to have_content("#{@game.player_1.name} attacked #{@game.player_2.name}!!!")
+      expect(page).to have_content("Chris attacked Ben")
     end
-    
+
     scenario "reduce player2 hit points by 10" do
       sign_in_and_play
       click_button("Attack")
-      expect(page).to have_content("#{@game.player_2.name}: 90HP")
+      expect(page).to have_content("90HP")
     end
   end
